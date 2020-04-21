c79303fa5e9bc3e35d1fbbb38c4330c85b4edaf0
nilsnolde <nils.nolde@gmail.com>
2020-04-21 19:42:17 +0200

    more test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,11 +9,11 @@ if [ "${cmd}" == 'initialize' ]; then
   if test -f ${OSM_FILE}; then
     echo "OSM_FILE is empty"
   fi
-  osm2pgsql -s -C 2000 -c -l --hstore --hstore-match-only --style /osm_updater/config/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -U $POSTGRES_USER /osm_updater/osm/${OSM_FILE}
+  osm2pgsql -s -C 2000 -c -l --hstore --hstore-match-only --style /osm_updater/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -U $POSTGRES_USER /osm_updater/osm/${OSM_FILE}
 elif [ "${cmd}" == 'update-db' ]; then
   osmosis --read-replication-interval workingDirectory="${OSMOSIS_DIR}" --simplify-change --write-xml-change $FILENAME
-  python3 /osm_updater/regional/trim_osc.py --password -d $OSM_DB --user $POSTGRES_USER --host $POSTGRES_HOST --port $POSTGRES_PORT -p "/osm_updater/config/polys/liechtenstein.poly" -z $FILENAME $FILENAME
-  osm2pgsql -s -C 2000 --append -l --hstore --hstore-match-only --style /osm_updater/config/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -P POSTGRES_PORT -U $POSTGRES_USER $FILENAME
+  python3 /osm_updater/regional/trim_osc.py --password -d $OSM_DB --user $POSTGRES_USER --host $POSTGRES_HOST --port $POSTGRES_PORT -p "/osm_updater/polys/liechtenstein.poly" -z $FILENAME $FILENAME
+  osm2pgsql -s -C 2000 --append -l --hstore --hstore-match-only --style /osm_updater/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -P $POSTGRES_PORT -U $POSTGRES_USER $FILENAME
 elif [ -n "${cmd}" ]; then
   echo "Command ${cmd} not recognized"
   exit 1
