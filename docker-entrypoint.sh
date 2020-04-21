ce5377d78e13accf62443139cb554028d470611b
nilsnolde <nils.nolde@gmail.com>
2020-04-21 19:18:11 +0200

    next test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ if [ "${cmd}" == 'initialize' ]; then
   if test -f ${OSM_FILE}; then
     echo "OSM_FILE is empty"
   fi
-  osm2pgsql -s -C 2000 -c -l --hstore --hstore-match-only --style /map_uploader/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -U $POSTGRES_USER /map_uploader/osm/$OSM_FILE
+  osm2pgsql -s -C 2000 -c -l --hstore --hstore-match-only --style /map_uploader/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -U $POSTGRES_USER /map_uploader/osm/${OSM_FILE}
 elif [ "${cmd}" == 'update-db' ]; then
   osmosis --read-replication-interval workingDirectory="${OSMOSIS_DIR}" --simplify-change --write-xml-change $FILENAME
   python3 /osm_updater/regional/trim_osc.py --password -d $OSM_DB --user $POSTGRES_USER --host $POSTGRES_HOST --port $POSTGRES_PORT -p "${OSMOSIS_DIR}/switzerland.poly" -z $FILENAME $FILENAME
@@ -22,4 +22,4 @@ elif [ -n "${cmd}" ]; then
 fi
 
 # Keep docker running easy
-exec "$@"
+#exec "$@"
