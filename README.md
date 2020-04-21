957e287a20e89f7e913a91869fb9b24d82f0ca95
nilsnolde <nils.nolde@gmail.com>
2020-04-21 20:30:19 +0200

    test

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ The project relies on the presence of a PostgreSQL database which has the `hstor
 It's easiest to use Docker to install the program:
 
 `docker install`
+
+
+## Usage
+
+It's easiest to use via `docker-compose.yml`.
+
+### Initial import of OSM data
+
+Execute the following command after `init`ing the DB and add `./state.txt` as a volume to `/osm_updater/osmosis`:
+
+```bash
+wget "https://replicate-sequences.osm.mazdermind.de/?"`date -u +"%Y-%m-%d"`"T00:00:00Z" -O ./state.txt
+```
+
+### Environment Variables
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,12 @@ export PGPASSWORD=$POSTGRES_PASS
 OSMOSIS_DIR=/osm_updater/osmosis
 FILENAME=/osm_updater/tmp/osmchange.osm.gz
 
-if [ "${cmd}" == 'initialize' ]; then
+if [ "${cmd}" == 'init' ]; then
   if test -f ${OSM_FILE}; then
     echo "OSM_FILE is empty"
   fi
   osm2pgsql -s -C 2000 -c -l --hstore --hstore-match-only --style /osm_updater/styles/$TAG_SET -d $OSM_DB -H $POSTGRES_HOST -U $POSTGRES_USER /osm_updater/osm/${OSM_FILE}
   # update the state.txt for osmosis
-  wget "https://replicate-sequences.osm.mazdermind.de/?"`date -u +"%Y-%m-%d"`"T00:00:00Z" -O $OSMOSIS_DIR/state.txt
 elif [ "${cmd}" == 'update-db' ]; then
   osmosis --read-replication-interval workingDirectory="${OSMOSIS_DIR}" --simplify-change --write-xml-change $FILENAME
   python3 /osm_updater/regional/trim_osc.py --password -d $OSM_DB --user $POSTGRES_USER --host $POSTGRES_HOST --port $POSTGRES_PORT -p "/osm_updater/polys/liechtenstein.poly" -z $FILENAME $FILENAME
@@ -24,4 +23,4 @@ elif [ -n "${cmd}" ]; then
 fi
 
 # Keep docker running easy
-#exec "$@"
+exec "$@"
